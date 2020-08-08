681bbe5eecd1bc00b7a862f94f9cb939659c23a6
Nadeera Abeykoon <nabeykoon@gmail.com>
2020-08-08 13:53:48 +0530

    After adding different action pages

--- src/main/java/com/webapp/pages/BasePageObject.java
+++ src/main/java/com/webapp/pages/BasePageObject.java
@@ -148,4 +148,39 @@ public class BasePageObject {
         JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
         jsExecutor.executeScript("window.scrollTo(0, document.body.scrollHeight)");
     }
+
+    /** Drag 'from' element to 'to' element */
+    protected void performDragAndDrop(By from, By to) {
+
+        //Due to selenium bug following approach may not work in some pages
+        // Actions action = new Actions(driver);
+        // action.dragAndDrop(find(from), find(to)).build().perform();
+
+        // Work around using JS executor to drag and drop
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        jsExecutor.executeScript(
+                "function createEvent(typeOfEvent) {\n" + "var event =document.createEvent(\"CustomEvent\");\n"
+                        + "event.initCustomEvent(typeOfEvent,true, true, null);\n" + "event.dataTransfer = {\n"
+                        + "data: {},\n" + "setData: function (key, value) {\n" + "this.data[key] = value;\n" + "},\n"
+                        + "getData: function (key) {\n" + "return this.data[key];\n" + "}\n" + "};\n"
+                        + "return event;\n" + "}\n" + "\n" + "function dispatchEvent(element, event,transferData) {\n"
+                        + "if (transferData !== undefined) {\n" + "event.dataTransfer = transferData;\n" + "}\n"
+                        + "if (element.dispatchEvent) {\n" + "element.dispatchEvent(event);\n"
+                        + "} else if (element.fireEvent) {\n" + "element.fireEvent(\"on\" + event.type, event);\n"
+                        + "}\n" + "}\n" + "\n" + "function simulateHTML5DragAndDrop(element, destination) {\n"
+                        + "var dragStartEvent =createEvent('dragstart');\n"
+                        + "dispatchEvent(element, dragStartEvent);\n" + "var dropEvent = createEvent('drop');\n"
+                        + "dispatchEvent(destination, dropEvent,dragStartEvent.dataTransfer);\n"
+                        + "var dragEndEvent = createEvent('dragend');\n"
+                        + "dispatchEvent(element, dragEndEvent,dropEvent.dataTransfer);\n" + "}\n" + "\n"
+                        + "var source = arguments[0];\n" + "var destination = arguments[1];\n"
+                        + "simulateHTML5DragAndDrop(source,destination);",
+                find(from), find(to));
+    }
+
+    /** Perform mouse hover over element */
+    protected void hoverOverElement(WebElement element) {
+        Actions action = new Actions(driver);
+        action.moveToElement(element).build().perform();
+    }
 }
