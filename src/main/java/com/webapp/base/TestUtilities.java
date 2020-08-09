0cf635ea820ef33a174b79addfbbc34267f0009f
Nadeera Abeykoon <nabeykoon@gmail.com>
2020-08-09 19:18:14 +0530

    Complete Data driven capabilities

--- src/main/java/com/webapp/base/TestUtilities.java
+++ src/main/java/com/webapp/base/TestUtilities.java
@@ -1,5 +1,7 @@
 package com.webapp.base;
 
+import org.testng.annotations.DataProvider;
+
 public class TestUtilities extends BaseTest {
 
     //STATIC SLEEP IF REQUIRED FOR DEBUGGING. NOT RECOMMENDED TO USE IN TESTS
@@ -9,5 +11,14 @@ public class TestUtilities extends BaseTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    @DataProvider(name = "files")
+    protected static Object[][] files() {
+        return new Object[][]{
+                {1, "Intel-logo.jpg"},
+                {2, "download.png"},
+                {3, "text.txt"}
+        };
     }
 }
--- src/test/java/com/webapp/loginpagetests/NegativeLogInTests.java
+++ src/test/java/com/webapp/loginpagetests/NegativeLogInTests.java
@@ -1,20 +1,25 @@
 package com.webapp.loginpagetests;
 
+import com.webapp.base.CsvDataProviders;
 import com.webapp.base.TestUtilities;
 import com.webapp.pages.LoginPage;
 import com.webapp.pages.WelcomePage;
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.Map;
+
 public class NegativeLogInTests extends TestUtilities {
 
-
-    @Parameters({"username", "password", "expectedMessage"})
-    @Test(priority = 1)
-    public void negativeTest(String username, String password, String expectedErrorMessage) {
-        log.info("Starting negativeTest");
-
+    @Test(priority = 1, dataProvider = "csvReader", dataProviderClass = CsvDataProviders.class)
+    public void negativeLogInTest(Map<String, String> testData) {
+        //Data
+        String no = testData.get("no");
+        String username = testData.get("username");
+        String password = testData.get("password");
+        String expectedErrorMessage = testData.get("expectedMessage");
+        String description = testData.get("description");
+        log.info("Starting negativeLogInTest #" + no + " for " + description);
         // open main page
         WelcomePage welcomePage = new WelcomePage(driver, log);
         welcomePage.openPage();
